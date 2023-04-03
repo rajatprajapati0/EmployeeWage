d398e7471597b9d01fd5bc0597273a03e97b0140
Rajat <rajatprajapati09@gmail.com>
2023-04-04 03:02:06 +0530

    checking employee is prsent or absent

--- Program.cs
+++ Program.cs
@@ -4,9 +4,22 @@ namespace EmployeeWage
 {
     internal class Program
     {
+        const int present = 1; 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage Program");
+            Random rnd = new Random();
+
+            int check=rnd.Next(0,2); 
+            if (check==present) 
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else 
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
         }
     }
 }
