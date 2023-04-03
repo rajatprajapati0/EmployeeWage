9088037521241d2e6825a770d17fa630063a61f3
Rajat <rajatprajapati09@gmail.com>
2023-04-04 03:18:44 +0530

    calculatnig daily wage also for half day

--- Program.cs
+++ Program.cs
@@ -5,25 +5,29 @@ namespace EmployeeWage
     internal class Program
     {
         const int wagePerHour = 20;
-        const int present = 1; 
-      
+        const int presentForFullDay = 1; 
+        const int presentForHalflDay = 2;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage Program");
             Random rnd = new Random();
-            int check=rnd.Next(0,2);
+            int check=rnd.Next(0,3);
             int perDayhour = 0;
 
-            if (check==present) 
+            if (check== presentForFullDay) 
             {
                 perDayhour = 8;
+            }
+           else if (check == presentForHalflDay)
+            {
+                perDayhour = 4;
             }
             else 
             {
               perDayhour =0;    
             }
             int dailyWage = perDayhour * wagePerHour;
-            Console.WriteLine($"Employee Per Day Wage :{dailyWage}");
+            Console.WriteLine($"Employee Per Day Wage :{dailyWage}$");
 
         }
     }
