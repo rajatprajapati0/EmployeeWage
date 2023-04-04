adca1255029ee4acb503b2841111db0533698e1f
Rajat <rajatprajapati09@gmail.com>
2023-04-04 07:25:49 +0530

    calculating wage for max 20 day in a month

--- Program.cs
+++ Program.cs
@@ -5,29 +5,34 @@ namespace EmployeeWage
     internal class Program
     {
         const int wagePerHour = 20;
-
+        const int maxWorkingDay = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage Program");
-            Random rnd = new Random();
-            int check=rnd.Next(0,3);
+            Random rnd = new Random();         
             int perDayhour = 0;
+            int totalMonthlyWage = 0;
 
-            switch (check)
+            for (int i = 0; i < maxWorkingDay; i++) 
             {
-                case 0:
-                    perDayhour = 0;
-                    break;
-                case 1:
-                    perDayhour = 8;
-                    break;
-                case 2:
-                    perDayhour = 4;
-                    break;
+                int check = rnd.Next(0, 3);
+                switch (check)
+                {
+                    case 0:
+                        perDayhour = 0;
+                        break;
+                    case 1:
+                        perDayhour = 8;
+                        break;
+                    case 2:
+                        perDayhour = 4;
+                        break;
+                }
+                int dailyWage = perDayhour * wagePerHour;
+                 totalMonthlyWage += dailyWage;
             }
-            int dailyWage = perDayhour * wagePerHour;
-            Console.WriteLine($"Employee Per Day Wage :{dailyWage}$");
-
+                        
+            Console.WriteLine($"EmployeeWage of a month :{totalMonthlyWage}$");
         }
     }
 }
