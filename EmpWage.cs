5f37dc077148829de321a33d466a7c8026b84cfd
Rajat <rajatprajapati09@gmail.com>
2023-04-09 16:00:11 +0530

    Refactored Code to write a Class Method to Compute employeeWage

---
+++ EmpWage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+    internal class EmpWage
+    {
+        const int wagePerHour = 20;
+        const int maxWorkingDay = 20;
+        const int maxHour = 100;
+        public void computeEmployeeWage() 
+        {
+            int perDayhour = 0;
+            int totalMonthlyWage = 0;
+            int totalHour = 0;
+            int totalDay = 0;
+            Random rnd = new Random();
+
+            while (totalDay < maxWorkingDay && totalHour < maxHour)
+            {
+                totalDay++;
+                int check = rnd.Next(0, 3);
+                switch (check)
+                {
+                    case 0:
+                        perDayhour = 0;
+                        break;
+                    case 1:
+                        perDayhour = 8;
+                        break;
+                    case 2:
+                        perDayhour = 4;
+                        break;
+                }
+                totalHour += perDayhour;
+                int dailyWage = perDayhour * wagePerHour;
+                totalMonthlyWage += dailyWage;
+            }
+
+            Console.WriteLine($"EmployeeWage of a month :{totalMonthlyWage}$\ntotal day :{totalDay}\ntotal hour:{totalHour}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,40 +4,13 @@ namespace EmployeeWage
 {
     internal class Program
     {
-        const int wagePerHour = 20;
-        const int maxWorkingDay = 20;
-        const int maxHour = 100;
+      
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage Program");
-            int perDayhour = 0;
-            int totalMonthlyWage = 0;
-            int totalHour = 0;
-            int totalDay = 0;
-            Random rnd = new Random();
-
-            while (totalDay < maxWorkingDay && totalHour<maxHour)           
-            {
-                    totalDay++;
-                int check = rnd.Next(0, 3);
-                switch (check)
-                {
-                    case 0:
-                        perDayhour = 0;
-                        break;
-                    case 1:
-                        perDayhour = 8;
-                        break;
-                    case 2:
-                        perDayhour = 4;
-                        break;
-                }
-                totalHour += perDayhour;
-                int dailyWage = perDayhour * wagePerHour;
-                totalMonthlyWage += dailyWage;
-            }
-                        
-            Console.WriteLine($"EmployeeWage of a month :{totalMonthlyWage}$\ntotal day :{totalDay}\ntotal hour:{totalHour}");
+            EmpWage emp = new EmpWage();
+            emp.computeEmployeeWage();
+            
         }
     }
 }
